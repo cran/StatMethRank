449ce860e7045743bc9e92b7f750a8f4acdbc165
Li Qinglong <liqinglong0830@163.com>
2014-05-27 00:00:00 +0000

    version 1.0

---
+++ R/Croon.R
@@ -0,0 +1,17 @@
+#' Croon data on goals of the Government
+#'
+#' M.A. Croon conducted a survey in Germany on the political goals on the Government.
+#' The data set contains 2262 rankings, in the aggregated form, of four goals.
+#' 
+#' \itemize{
+#'   \item 1, Maintain order in nation
+#'   \item 2, Give people more say in Government decisions
+#'   \item 3, Fight rising prices
+#'   \item 4, Protect freedom of speech
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A matrix with 24 rows and 5 columns
+#' @name Croon
+NULL
